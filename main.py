273833b2186c4a97080be879d7eb8e1cd365a47c
Ayesha Amjad <81661022+Ayesha21410@users.noreply.github.com>
2022-06-30 22:41:52 +0000

    inserting background

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 #Imports module
-from tkinter import *  
+from tkinter import *  #represents all the functions and built-in modules in the tkinter library
 from PIL import Image, ImageTk # for Images
 from tkinter import messagebox  # for error messages
-import random
-names_list = [] 
-asked=[]
+import random  # random function incode for the questions.
+names_list = [] #List used to append names in results pages.
+asked=[] #List used to append the questions.
  
 #Component 1
 # inserting first class which is QuizStarter.
 class QuizStarter:
+  def __init__(self, parent):#constructor, The __init__() function is called automatically every time the class is being used to create a new object.
+  

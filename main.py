507cb4d4b04e7a6d87049cda172ddef6663a0768
Ayesha Amjad <81661022+Ayesha21410@users.noreply.github.com>
2022-06-30 22:41:16 +0000

    inserting constructor

--- main.py
+++ main.py
@@ -3,3 +3,9 @@ from tkinter import *
 from PIL import Image, ImageTk # for Images
 from tkinter import messagebox  # for error messages
 import random
+names_list = [] 
+asked=[]
+ 
+#Component 1
+# inserting first class which is QuizStarter.
+class QuizStarter:
